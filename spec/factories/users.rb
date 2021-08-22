e75979516591043480199fa412c638eb5f620af4
hamana-n <hamana.n.1027@icloud.com>
2021-08-23 05:33:43 +0900

    修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation {password}
     first_name            {'試験'}
     last_name             {'試験'}
