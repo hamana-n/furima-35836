2a68cde97b4e335583381bd04cbcd5edf37090c8
hamana-n <hamana.n.1027@icloud.com>
2021-09-01 20:13:29 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
 
 
@@ -36,6 +36,13 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    redirect_to root_path unless current_user.id == @item.user_id
+     if @item.destroy
+      redirect_to root_path
     end
   end
 
