7de8883910c21996429c584db1347e524275cc15
hamana-n <hamana.n.1027@icloud.com>
2021-09-10 14:36:19 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
       end
    end
   
-   validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 99_999_999 }
+   validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
 
   
